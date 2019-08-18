386182d7d5e3872e4facf08fbec049888dcfde97
Jan Bílek <honza@elision.design>
2019-08-18 19:22:59 +0200

    Add tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+
+// Purge unused CSS
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Paths to all of the template files in the project
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+    './src/**/*.tsx',
+  ],
+  whitelist: ['html', 'body'],
+  // Regular expression for extracting CSS classes
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
 module.exports = {
-  plugins: {
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : [],
+  ],
 };
---
+++ src/views/Feed.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>This is an article feed</h1>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Feed',
+});
+</script>
